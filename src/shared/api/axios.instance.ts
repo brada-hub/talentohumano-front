ebc953e038989fa7658c7c6a5ca2b98db47aa34e
BRADA-HUB <padillasilesbrayandavid@gmail.com>
2026-04-11 17:24:57 -0400

    Sincronización de cambios y ajustes de producción

--- src/shared/api/axios.instance.ts
+++ src/shared/api/axios.instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.DEV ? 'http://localhost:8000/api' : 'https://api.sigeth.xpertiaplus.com/api',
   timeout: 10000,
 });
 
